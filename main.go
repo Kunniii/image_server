f715f6f814b606bf21da7048f283bfb424a4c228
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-08-17 16:37:06 +0700

    no need to cleanup

--- main.go
+++ main.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/kunniii/image_server/routes"
-	"github.com/kunniii/image_server/utils"
 
 	"github.com/gin-gonic/gin"
 )
-
-// startCleanupRoutine periodically cleans up old files
-func startCleanupRoutine() {
-	ticker := time.NewTicker(24 * time.Hour) // Run cleanup every 24 hours
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			if err := utils.CleanUpOldFiles(); err != nil {
-				// Log or handle the error
-				// For simplicity, we're just printing it here
-				println("Error cleaning up old files:", err.Error())
-			}
-		}
-	}
-}
 
 func main() {
 	r := gin.Default()
@@ -39,9 +20,6 @@ func main() {
 
 	// Ensure the images directory exists
 	os.MkdirAll("images", os.ModePerm)
-
-	// Start a cleanup routine to remove old files every 24 hours
-	go startCleanupRoutine()
 
 	// Start the server on port 8080
 	r.Run(":8080")
